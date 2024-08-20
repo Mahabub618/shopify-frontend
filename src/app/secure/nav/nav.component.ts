f7e13e729971dc3cd310eb27f933033fb43060d2
Mahabub <mahabub.rahman@tigerit.com>
2024-08-20 12:56:07 +0600

    Implement search list ui

--- src/app/secure/nav/nav.component.ts
+++ src/app/secure/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {User} from "../../interfaces/user";
 import {ApiService} from "../../services/api.service";
 
@@ -8,12 +8,40 @@ import {ApiService} from "../../services/api.service";
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  @ViewChild('searchList') searchList: ElementRef;
   user: User;
+  list: any = [];
   constructor(private apiService: ApiService) {
   }
   ngOnInit() {
     this.apiService.user().subscribe((user: User) => {
       this.user = user;
     })
+  }
+  onTextareaFocus() {
+    // console.log('#255 searchList is focused');
+    this.showSearchSuggestionList();
+  }
+  onTextareaBlur(event: FocusEvent) {
+    if (
+      !this.searchList.nativeElement.contains(event.relatedTarget as Node) &&
+      event.relatedTarget !== document.querySelector('.search-text-area')
+    ) {
+      this.hideSearchSuggestionList();
+    }
+  }
+  showSearchSuggestionList() {
+    this.searchList.nativeElement.style.display = 'block';
+  }
+  hideSearchSuggestionList() {
+    this.searchList.nativeElement.style.display = 'none';
+  }
+  onInputChange(event?: any) {
+    // console.log('#255 event', event);
+    // this.searchText = $event.target.value;
+    // this.getSearchSuggestionList($event);
+  }
+  onProductSelect(product: any) {
+    // console.log('#255 product', product);
   }
 }
